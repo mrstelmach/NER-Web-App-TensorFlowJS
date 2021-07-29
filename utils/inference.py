1350b5629e965ead560474c3dd119116d12f8bcc
mst <mst@mst-srv.NAT.warszawa.vectranet.pl>
2021-07-29 18:53:09 +0200

    modified script for inference

--- utils/inference.py
+++ utils/inference.py
@@ -3,11 +3,18 @@
 """Running model in inference mode."""
 
 
+import argparse
+import os
 from dataclasses import dataclass
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import joblib
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras import Model as KerasModel
 
+from model_tf.model_tuner import tf_set_memory_growth
 from utils.text import CustomTokenizer, TextPreprocessor
 
 
@@ -31,3 +38,24 @@ class getPredictedNER:
                       in zip(input_tokens, pred_cls)]
         
         return ner_output
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(
+        description='Process txt file with NER model.')
+    parser.add_argument('file', help='txt file for model input')
+    args = parser.parse_args()
+    with open (args.file) as sample_file:
+        sample = sample_file.read()
+    
+    tf_set_memory_growth()
+    
+    text_prc = joblib.load('inference/text_preprocessor.joblib')
+    tokenizer = joblib.load('inference/tokenizer.joblib')
+    model = tf.keras.models.load_model('inference/h5_model/model.h5')
+    label_encoder = joblib.load('inference/label_encoder.joblib')
+
+    get_ner = getPredictedNER(text_prc, tokenizer, model, label_encoder.classes_.tolist())
+    print(get_ner(sample))
+    
